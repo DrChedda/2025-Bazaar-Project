63d8796cda95bfcbeffb24a9152091e7f920ebc0
Crypto <cryptowecrypto@gmail.com>
2025-09-18 14:11:10 -0400

    Create Program.cs

---
+++ Game/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Game
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("GAME!!!");
+      // TODO: Initialize engine and JS logic, aka BUILD DA GAME.
+    }
+  }
+}
