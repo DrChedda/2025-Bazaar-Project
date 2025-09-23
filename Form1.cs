bcb16da1a5975d4dff55743a5ae7be1f178fead8
Crypto <cryptowecrypto@gmail.com>
2025-09-23 10:13:19 -0400

    Added some stuff

--- Form1.cs
+++ Form1.cs
@@ -9,22 +9,36 @@ namespace GCHS_2025_Bazaar
         {
             InitializeComponent();
             this.Text = "GCHS 2025 Bazaar";
-            this.Width = 800;
-            this.Height = 600;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
             this.StartPosition = FormStartPosition.CenterScreen;
 
             Button startButton = new Button();
             startButton.Text = "Start Game";
             startButton.Width = 200;
             startButton.Height = 60;
-            startButton.Left = (this.ClientSize.Width - startButton.Width) / 2;
-            startButton.Top = (this.ClientSize.Height - startButton.Height) / 2;
             startButton.Click += StartButton_Click;
 
-            this.Controls.Add(startButton);
-        }
+            // Center the button after the form is maximized
+            this.Load += (s, e) =>
+            {
+                startButton.Left = (this.ClientSize.Width - startButton.Width) / 2;
+                startButton.Top = (this.ClientSize.Height - startButton.Height) / 2;
+            };
+            startButton.Anchor = AnchorStyles.None;
 
-        private void StartButton_Click(object sender, EventArgs e)
+            this.Controls.Add(startButton);
+            this.KeyPreview = true;
+            this.KeyDown += GameForm_KeyDown;
+        }
+        private void GameForm_KeyDown(object? sender, KeyEventArgs e)
+        {
+            if (e.Shift && e.KeyCode == Keys.Escape)
+            {
+                Environment.Exit(0);
+            }
+        }
+        private void StartButton_Click(object? sender, EventArgs e)
         {
             this.Hide();
             GameForm gameForm = new GameForm();
--- GameForm.cs
+++ GameForm.cs
@@ -14,37 +14,61 @@ namespace GCHS_2025_Bazaar
         {
             InitializeComponent();
             this.Text = "Scene 1";
-            this.Width = 800;
-            this.Height = 600;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
             this.StartPosition = FormStartPosition.CenterScreen;
 
             sceneLabel = new Label();
             sceneLabel.Text = "You wake up in a strange room. Two paths lie ahead.";
             sceneLabel.Width = 600;
             sceneLabel.Height = 100;
-            sceneLabel.Top = 50;
-            sceneLabel.Left = 100;
             sceneLabel.Font = new Font("Segoe UI", 14);
             this.Controls.Add(sceneLabel);
 
             choiceA = new Button();
             choiceA.Text = "Take the left path";
             choiceA.Width = 250;
-            choiceA.Top = 200;
-            choiceA.Left = 100;
             choiceA.Click += ChoiceA_Click;
             this.Controls.Add(choiceA);
 
             choiceB = new Button();
             choiceB.Text = "Take the right path";
             choiceB.Width = 250;
-            choiceB.Top = 200;
-            choiceB.Left = 400;
             choiceB.Click += ChoiceB_Click;
             this.Controls.Add(choiceB);
+
+            this.Load += (s, e) =>
+            {
+                // Center the label at the top
+                sceneLabel.Left = (this.ClientSize.Width - sceneLabel.Width) / 2;
+                sceneLabel.Top = 50;
+
+                // Center the buttons horizontally, spaced apart
+                int spacing = 40;
+                int totalWidth = choiceA.Width + spacing + choiceB.Width;
+                int startX = (this.ClientSize.Width - totalWidth) / 2;
+                int buttonY = sceneLabel.Bottom + 50;
+
+                choiceA.Left = startX;
+                choiceA.Top = buttonY;
+
+                choiceB.Left = startX + choiceA.Width + spacing;
+                choiceB.Top = buttonY;
+            };
+
+            this.KeyPreview = true;
+            this.KeyDown += GameForm_KeyDown;
         }
 
-        private void ChoiceA_Click(object sender, EventArgs e)
+        private void GameForm_KeyDown(object? sender, KeyEventArgs e)
+        {
+            if (e.Shift && e.KeyCode == Keys.Escape)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void ChoiceA_Click(object? sender, EventArgs e)
         {
             sceneLabel.Text = "You take the left path and find a key on the ground.";
             choiceA.Text = "Pick up the key";
@@ -68,13 +92,13 @@ namespace GCHS_2025_Bazaar
             };
         }
 
-        private void ChoiceB_Click(object sender, EventArgs e)
+        private void ChoiceB_Click(object? sender, EventArgs e)
         {
             sceneLabel.Text = "You fall into a trap! Game over.";
             choiceA.Visible = false;
             choiceB.Visible = false;
         }
-
+        
         private void InitializeComponent()
         {
 
